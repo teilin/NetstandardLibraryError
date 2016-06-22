1bba17ac3c7994966f53f2b79421b96f9d6e482e
Teis Lindemark <teis.lindemark@gmail.com>
2016-06-22 22:08:12 +0200

    sd

--- KodeverkRepository.cs
+++ KodeverkRepository.cs
@@ -5,15 +5,9 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 //using Dapper;
 
-namespace ACOS.Levekaar.KodeverkService.Repositories
+namespace Repositories
 {
   public class KodeverkRepository
   {
-      private readonly AppSettings _appSettings;
-
-      public KodeverkRepository(AppSettings appSettings)
-      {
-          _appSettings = appSettings;
-      }
   }
 }
